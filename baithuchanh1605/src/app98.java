f3f263db23b9bc6769d704be37340710dc965c41
NguyenAnhTuan <nguyenanhtuan16402@gmail.com>
2022-05-19 09:25:51 +0700

    nguyenanhtuan2021050892

---
+++ baithuchanh1605/src/app98.java
@@ -0,0 +1,16 @@
+import java.util.TreeMap;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class App98 {
+    public static void main(String[] args) {
+
+        TreeMap<String, Double> treeMap1 = new TreeMap<>();
+       
+        Map<Float, Double> map = new HashMap<>();
+
+        TreeMap<Float, Integer> treeMap2 =new TreeMap<>();
+    }
+    
+}
